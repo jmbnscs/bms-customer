63309a3705c7439a3f55e4c448dab90181022bdf
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2023-01-21 14:08:20 +0800

    update

--- public/views/home.php
+++ public/views/home.php
@@ -13,14 +13,14 @@
         <h1 data-aos="fade-up">Become one of the many customers choosing GSTech</h1>
         <h2 data-aos="fade-up" data-aos-delay="400">service for your home to enjoy
                                                     fast but cheaper internet!</h2>
-        <div data-aos="fade-up" data-aos-delay="800">
+        <!-- <div data-aos="fade-up" data-aos-delay="800">
           <div class="text-center text-lg-start">
             <a href="#inquire" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
               <span>Get Started</span>
               <i class="bi bi-arrow-right"></i>
             </a>
           </div>
-        </div>
+        </div> -->
       </div>
 
       <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
