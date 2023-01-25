a8b47c11f008c63dc65951c78e5e694aca8c0319
Jm <jmsbuensuceso@iskolarngbayan.pup.edu.ph>
2023-01-25 17:21:57 +0800

    added email validation to inquiry form
    Co-authored-by: BugCasterSenpai <BugCasterSenpai@users.noreply.github.com>

--- public/js/home.js
+++ public/js/home.js
@@ -8,32 +8,49 @@ $('form').on('submit', function (e) {
     $('#submit-inquiry-btn').prop('disabled', true);
     $('#submit-inquiry-btn').append('&emsp;<i class="fa fa-circle-o-notch fa-spin"></i>');
 
-    $.ajax({
-        type : 'POST',
-        url : DIR_APP_HOME + 'send_inquiry.php',
-        data: $( this ).serialize(),
-        success : function (data) {
-            if (data == 'success') {
-                setTimeout ( () => {
-                        $('#inquire-data').trigger("reset");
-                        $('#submit-inquiry-btn').prop('disabled', false);
-                        $('#submit-inquiry-btn').text('Submit');
-                        toastr.success('Your inquiry has been sent!', 'Success');
-                    },2000
-                );
+    if (validateEmail($('#inquire_email').val())) {
+        $.ajax({
+            type : 'POST',
+            url : DIR_APP_HOME + 'send_inquiry.php',
+            data: $( this ).serialize(),
+            success : function (data) {
+                if (data == 'success') {
+                    setTimeout ( () => {
+                            $('#inquire-data').trigger("reset");
+                            $('#submit-inquiry-btn').prop('disabled', false);
+                            $('#submit-inquiry-btn').text('Submit');
+                            toastr.success('Your inquiry has been sent!', 'Success');
+                        },2000
+                    );
+                }
+                else {
+                    setTimeout ( () => {
+                            $('#submit-inquiry-btn').prop('disabled', false);
+                            $('#submit-inquiry-btn').text('Submit');
+                            toastr.error('Some error has occured, please try again later.', 'Oops!');
+                            // toastr.error(data);
+                        },2000
+                    );
+                }
             }
-            else {
-                setTimeout ( () => {
-                        $('#submit-inquiry-btn').prop('disabled', false);
-                        $('#submit-inquiry-btn').text('Submit');
-                        toastr.error('Some error has occured, please try again later.', 'Oops!');
-                        // toastr.error(data);
-                    },2000
-                );
-            }
-        }
-    });
+        });
+    }
+    else {
+        toastr.error('Your email is not valid, please try using another email.', 'Oops!');
+        $('#inquire_email').val(null);
+        $('#submit-inquiry-btn').prop('disabled', false);
+        $('#submit-inquiry-btn').text('Submit');
+    }
+
+    
 });
+
+function validateEmail(inquirer_email) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (validRegex.test(inquirer_email)) return true;
+    return false;
+}
 
 toastr.options = {
     "closeButton": true,
